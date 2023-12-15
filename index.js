14b0a230ad2c428d9a4cb8c85ae7d50ef1d5aade
Md yasen rafe <mdyasenrafe@gmail.com>
2023-12-15 19:49:30 +0600

    payload issue resolved

--- index.js
+++ index.js
@@ -21,11 +21,21 @@ const http = require("http").Server(app);
 
 // middle wares
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: "50mb", extended: true }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
+app.use(bodyParser.text({ limit: "200mb" }));
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb" }));
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
+
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
 
 // model connect
 mongoose.set("strictQuery", false);
